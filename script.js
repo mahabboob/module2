c9ed0027dd767f988141bc3f19d6c78a2a4998a9
mahaboob <starmehaboob@gmail.com>
2023-12-05 09:44:19 +0400

    initial commit

---
+++ script.js
@@ -0,0 +1,16 @@
+function validateform(e){
+    e.preventDefault();
+    console.log("form validated");
+    let username = document.getElementById("name").value;
+    let mailid = document.getElementById("mail").value;
+    let passsword = document.getElementById("password").value;
+    
+    console.log(username);
+    console.log(mailid);
+    console.log(passsword);
+
+    
+    
+    
+    }
+    
